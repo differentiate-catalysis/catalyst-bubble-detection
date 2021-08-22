89d64a43417fbb7c632560cbaea0461f1b62776b
Jim James <github@jimjam.es>
2021-08-22 17:25:52 -0400

    Correct targets in the case of degenerate instances

--- transforms.py
+++ transforms.py
@@ -72,15 +72,52 @@ def transform_boxes(image: torch.Tensor, target: Dict[str, torch.Tensor], transf
     out_boxes[:, 1] = torch.maximum(torch.amin(boxes[:, [1, 3, 5, 7]], dim=-1), torch.zeros(1))
     out_boxes[:, 2] = torch.minimum(torch.amax(boxes[:, [0, 2, 4, 6]], dim=-1), torch.tensor([width - 1]))
     out_boxes[:, 3] = torch.minimum(torch.amax(boxes[:, [1, 3, 5, 7]], dim=-1), torch.tensor([height - 1]))
-    # TODO: Remove negative boxes
-    # invalid_rows = torch.where(out_boxes < 0)
-    # if len(invalid_rows) > 0:
-        # invalid_rows = invalid_rows[0]
-        # rows = list(range(out_boxes.shape[0]))
-        # for i in invalid_rows:
-            # rows.remove(i)
+    # Remove degenerate boxes:
+    nonnegative_vals_tensor = torch.sum(out_boxes >= 0, dim=-1) == 4
+    nonnegative_width_tensor = (out_boxes[:, 2] - out_boxes[:, 0]) > 0
+    nonnegative_height_tensor = (out_boxes[:, 3] - out_boxes[:, 1]) > 0
+    total_tensor = nonnegative_height_tensor * nonnegative_vals_tensor * nonnegative_width_tensor
+    proper_instances = torch.where(total_tensor == 1)
+    out_boxes = out_boxes[proper_instances]
     areas = (out_boxes[:, 2] - out_boxes[:, 0]) * (out_boxes[:, 1] - out_boxes[:, 3])
     return areas, out_boxes
+
+def target_from_masks(masks: torch.Tensor) -> Dict[str, torch.Tensor]:
+    target = {}
+    # We can instances with no mask by summing and checking for a positive number
+    nonzero_instances = torch.where(torch.sum(masks, dim=[-1, -2]) > 0)[0]
+    if nonzero_instances.shape[0] > 0:
+        masks = masks[nonzero_instances]
+        # Now we need to check for masks where the max and min are the same
+        boxes = torch.empty((masks.shape[0], 4), dtype=torch.float32)
+        areas = torch.empty(masks.shape[0])
+        proper_instances = list(range(masks.shape[0]))
+        for i in range(masks.shape[0]):
+            rows, cols = torch.where(masks[i] == 1)
+            x_0 = torch.amin(cols)
+            y_0 = torch.amin(rows)
+            x_1 = torch.amax(cols)
+            y_1 = torch.amax(rows)
+            boxes[i, :] = torch.tensor([x_0, y_0, x_1, y_1], dtype=torch.float32)
+            areas[i] = (x_1 - x_0) * (y_1 - y_0)
+            # If an instance has width or height 0, discard it
+            if x_0 == x_1 or y_0 == y_1:
+                proper_instances.remove(i)
+        masks = masks[proper_instances]
+        boxes = boxes[proper_instances]
+        areas = areas[proper_instances]
+        target['labels'] = torch.ones(boxes.shape[0], dtype=torch.int64)
+        target['iscrowd'] = torch.zeros(boxes.shape[0], dtype=torch.uint8)
+    else:
+        masks = torch.tensor([[[]]], dtype=torch.uint8)
+        boxes = torch.tensor([[]], dtype=torch.float32)
+        areas = torch.tensor([], dtype=torch.float32)
+        target['labels'] = torch.tensor([], dtype=torch.int64)
+        target['iscrowd'] = torch.tensor([], dtype=torch.uint8)
+    target['masks'] = masks
+    target['boxes'] = boxes
+    target['areas'] = areas
+    return target
 
 class RandomRotation(T.RandomRotation):
     def forward(self, image:torch.Tensor, target: Optional[Dict[str, torch.Tensor]] = None) -> Tuple[torch.Tensor, Optional[Dict[str, torch.Tensor]]]:
@@ -90,30 +127,9 @@ class RandomRotation(T.RandomRotation):
         if target is not None:
             if 'masks' in target:
                 mask = F.rotate(target['masks'], degrees)
-                good_instances = torch.where(torch.sum(mask, dim=[-1, -2]) > 0)[0]
-                if good_instances.shape[0] > 0:
-                    mask = mask[good_instances]
-                    boxes = torch.empty((mask.shape[0], 4), dtype=torch.float32)
-                    areas = torch.empty(mask.shape[0])
-                    for i in range(mask.shape[0]):
-                        rows, cols = torch.where(mask[i] == 1)
-                        x_0 = torch.amin(cols)
-                        y_0 = torch.amin(rows)
-                        x_1 = torch.amax(cols)
-                        y_1 = torch.amax(rows)
-                        boxes[i, :] = torch.tensor([x_0, y_0, x_1, y_1], dtype=torch.float32)
-                        areas[i] = (x_1 - x_0) * (y_1 - y_0)
-                    target['labels'] = torch.ones(boxes.shape[0], dtype=torch.int64)
-                    target['iscrowd'] = torch.zeros(boxes.shape[0], dtype=torch.uint8)
-                else:
-                    mask = torch.tensor([[[]]], dtype=torch.uint8)
-                    boxes = torch.tensor([[]], dtype=torch.float32)
-                    areas = torch.tensor([], dtype=torch.float32)
-                    target['labels'] = torch.tensor([], dtype=torch.int64)
-                    target['iscrowd'] = torch.tensor([], dtype=torch.uint8)
-                target['masks'] = mask
-                target['boxes'] = boxes
-                target['areas'] = areas
+                corrected_target = target_from_masks(mask)
+                for key, value in corrected_target.items():
+                    target[key] = value
             else:
                 matrix = torch.tensor([[math.cos(rad), -math.sin(rad)], [math.sin(rad), math.cos(rad)]], dtype=torch.float32)
                 target['areas'], target['boxes'] = transform_boxes(image, target, matrix)

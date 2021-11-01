34c0a814f04cf3c997d8d9130729a18337df314b
NathanPruyne <nathan.pruyne@gmail.com>
2021-11-01 04:43:20 -0500

    Add saving boxes and masks and start of integrating color_splash

--- evaluate.py
+++ evaluate.py
@@ -13,6 +13,9 @@ from torch.nn import Module
 from torch.utils.data import DataLoader
 from torchvision.transforms.functional import to_pil_image
 from tqdm import tqdm
+import skimage.io
+import cv2
+import numpy as np
 
 from coco_eval import CocoEvaluator
 from coco_utils import get_coco_api_from_dataset
@@ -20,7 +23,7 @@ from coco_utils import get_coco_api_from_dataset
 from models import model_mappings
 from utils import Dataset, collate_fn
 
-def evaluate(model: Module, valid_loader: DataLoader, amp: bool, gpu: int, save_dir: str = None) -> Tuple[float, float, float]:
+def evaluate(model: Module, valid_loader: DataLoader, amp: bool, gpu: int, save_dir: str = None, test: bool = False) -> Tuple[float, float, float]:
     if gpu != -1:
         device = torch.device('cuda', gpu)
     else:
@@ -56,6 +59,19 @@ def evaluate(model: Module, valid_loader: DataLoader, amp: bool, gpu: int, save_
                     scores.append(nms_scores)
 
                     #TODO: Add export of boxes and masks
+                    if test:
+                        this_image = os.path.basename(os.listdir(valid_loader.dataset.patch_root)[j])
+                        if 'boxes' in output:
+                            box_cpu = output['boxes'].cpu()
+                            np.save(os.path.join(save_dir, 'boxes', this_image), box_cpu)
+                        if 'masks' in output:
+                            mask_cpu = output['masks'].cpu()
+                            np.save(os.path.join(save_dir, 'masks', this_image), mask_cpu)
+
+                        labeled = label_on_image(os.path.join(valid_loader.dataset.patch_root, this_image), output['boxes'])
+                        label_image = to_pil_image(labeled)
+                        label_image.save(os.path.join(save_dir, 'labeled_images', this_image))
+                    
 
                     # if 'masks' in output:
                         # masks = output['masks']
@@ -74,7 +90,7 @@ def evaluate(model: Module, valid_loader: DataLoader, amp: bool, gpu: int, save_
                 loss_dict = model(images, targets)
                 loss = sum(loss for loss in loss_dict.values()).item()
                 model.eval()
-            if not math.isfinite(loss):
+            if not math.isfinite(loss) and not test:
                 print('Loss is %s, stopping training' % loss)
                 if tune.is_session_enabled():
                     tune.report(loss=10000, iou=0, map=0)
@@ -179,6 +195,142 @@ def get_metrics(boxes: List[Tensor], scores: List[Tensor], targets: List[Tensor]
     mean_iou = iou.mean()
     return average_precisions, mean_iou
 
+def label_on_image(image_file, boxes):
+
+    maxBubblesPerFrame = 150                    # length of bubble tracking array
+    cols = 7                                    # width of bubble tracking array (number of datapoints for each bubble)
+    curr = np.zeros((maxBubblesPerFrame, cols)) # current frame bubble tracking array
+    prev = np.zeros((maxBubblesPerFrame, cols)) # previous frame bubble tracking array
+
+    lifetime_numBubbles = 1                     # counter for how many unique bubbles have been identified
+
+    precision_ID = 10                           # number of pixels the center of a bubble can drift from one frame to the next without triggering a new instance
+    precision_coalesce = 10                     # If a bubble dissapears, all bubbles within this number of pixels are assumed to have participated in coalesence
+    limit_nucleation = 23                       # If a newly identified bubble has a diameter greater than this number (in pixels) its growth is ignored (must not be a
+                                                # newly nucleated bubble, rather a large bubble that was lost in the previous frame and then re-recognized)
+
+    counter = 0
+
+    image = skimage.io.imread(image_file)
+
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+
+    for i in range(N):
+        if np.size(boxes[i]) == 0:
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]   # corners of bounding box. The accuracy of this algorithm could be greatly improved by  interpreting the masks directly as opposed to using the bounding box
+
+        D = int((x2+y2-x1-y1)/2)    # width of bounding box == diameter of bubble
+        center_x = int((x2+x1)/2)
+        center_y = int((y2+y1)/2)
+        match_plusminus = 0
+
+        image = np.array(image)
+        if D < 200 and i < maxBubblesPerFrame:    #bubble found!!
+            #Image editing
+            image = image.astype(np.uint8)
+        
+        
+# instance growth tracking: calculating the size +- for each bubble
+            # bubble tracking array
+            # | 0  | 1         | 2         | 3         | 4    | 5          | 6       |
+            # | ID | center X  | center Y  | Diameter  | New? | Coalesced? | Size +- |
+            curr[i, 1] = center_x
+            curr[i, 2] = center_y
+            curr[i, 3] = D
+
+            # go through the previous bubble tracking array and see if the current instance existed in the previous frame
+            foundmatch = 0
+            match_row = 0
+            for j in range(maxBubblesPerFrame):
+                if (np.abs(prev[j, 1]-center_x) + np.abs(prev[j, 2] - center_y)) < precision_ID:    # match based on center position
+                    foundmatch = 1
+                    curr[i, 0] = prev[j, 0] #assign id's
+                    match_row = j
+                    match_plusminus = prev[j, 6]
+                    break
+
+            # calculate Size +-
+            if not foundmatch: #newly identified bubble!
+                curr[i, 4] = 1 #set new flag
+                curr[i, 0] = lifetime_numBubbles
+                lifetime_numBubbles = lifetime_numBubbles + 1
+                if (D < limit_nucleation): # check if "new" bubble is small enough to actually be a new bubble as opposed to a lost + refound bubble.
+                    curr[i, 6] = 3.14/6*D*D*D # volume of a sphere
+                else:
+                    curr[i, 6] = 0 #change its +- to zero
+                    curr[i, 4] = 0 #remove its fresh tag
+
+            else: #not a new bubble
+                curr[i, 4] = 0
+                curr[i, 6] = 3.14/6*(D*D*D - prev[match_row, 3]*prev[match_row, 3]*prev[match_row, 3])      # incremental growth from the previous frame
+                    
+        else:
+            print("ignored (too big or too many)!")
+
+# Coalesence invalidation: if a bubble participates in a coalesence event, it should not contribute to the Size +-
+    for p in range(maxBubblesPerFrame):
+        # check if each bubble from prev still exists in curr
+        coalesce = 1
+        for c in range(maxBubblesPerFrame):
+            if(prev[p, 0] == curr[c, 0]):   #bubble still exists
+                coalesce = 0
+                break
+
+        if(coalesce):
+            for c2 in range(maxBubblesPerFrame):
+                dx = np.abs(prev[p, 1]-curr[c2, 1])
+                dy = np.abs(prev[p, 2]-curr[c2, 2])
+                dd = np.sqrt(dx*dx + dy*dy)             # distance between bubble centers
+                
+                avg_D = int((prev[p, 3] + curr[c2, 3])/2)
+                
+                if (dd - avg_D < precision_coalesce):   # If a bubble is within precision_coalesence of the dissapeared bubble, it should not contribute to Size +-
+                    curr[c2, 5] = 1   # coalesence flag
+#                   curr[c2, 6] = 0  # zeroth order
+                    curr[c2, 6] = match_plusminus  # first order (take the plus/minus from the last frame)
+
+
+# summing all the Size +- and assigning colors
+    sum = 0
+    for s in range(maxBubblesPerFrame):
+        sum = sum + curr[s, 6]
+
+        color = (255, 255, 255)
+        if (curr[s, 4]):  #New bubble!
+            color = (224, 230, 73)
+        elif (curr[s, 5]):  #coalesced bubble
+            color = (96, 66, 245)
+        elif (curr[s, 6] > 0): #growing bubble
+            color = (50, 168, 82)
+        elif(curr[s, 6] == 0): # size unchanged
+            color = (179, 247, 255)
+        else:   #shrinking bubble
+            color = (150, 0, 0)
+
+        # draw circle and write ID on frame
+        cv2.putText(image, str(int(curr[s, 0])), (int(curr[s, 1])-9, int(curr[s, 2])+7), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+        cv2.circle(image, (int(curr[s, 1]), int(curr[s, 2])), int(curr[s, 3]/2), color, 3)
+
+    with open("res.csv", 'a') as f: # log the total size+- for the counter-th frame
+        f.write(str(counter) + ", " + str(sum))
+        f.write("\n")
+
+
+# Resetting
+    if not os.path.isdir("./ID_table/"):
+        os.makedirs("./ID_table")
+    np.savetxt("./ID_table/" + str(counter) + ".csv", curr, delimiter=",")  # save all data to csv
+    prev = curr
+    curr = np.zeros((maxBubblesPerFrame, cols))
+    counter = counter + 1
+# end Resetting
+
+    return image
+
 def run_apply(args):
     test_dir = os.path.join(args.root, args.name, args.test_dir)
     if not (os.path.isdir(os.path.join(test_dir, 'patches')) and os.path.isdir(os.path.join(test_dir, 'targets'))):
@@ -204,9 +356,11 @@ def run_apply(args):
 
     save_dir = '%s/predictions/%s' % (test_dir, args.name)
     if not os.path.isdir(save_dir):
-        os.makedirs(save_dir)
+        os.makedirs(os.path.join(save_dir, 'boxes'))
+        os.makedirs(os.path.join(save_dir, 'masks'))
+        os.makedirs(os.path.join(save_dir, 'labeled_images'))
     
-    loss, iou, mAP = evaluate(model, test_loader, args.amp, gpu, save_dir)
+    loss, iou, mAP = evaluate(model, test_loader, args.amp, gpu, save_dir = save_dir, test=True)
     return loss, iou, mAP
     
 def run_metrics(args):

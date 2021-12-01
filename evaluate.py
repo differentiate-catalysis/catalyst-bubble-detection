265cf7c49aeea1830bee00c2ff32138239a7cfc5
Marcus Schwarting <meschw04@Marcuss-MacBook-Air.local>
2021-12-01 10:53:45 -0600

    Updates to eval script

--- evaluate.py
+++ evaluate.py
@@ -67,10 +67,10 @@ def evaluate(model: Module, valid_loader: DataLoader, amp: bool, gpu: int, save_
                         if 'masks' in output:
                             mask_cpu = output['masks'].cpu()
                             np.save(os.path.join(save_dir, 'masks', this_image), mask_cpu)
-
-                        labeled = label_on_image(os.path.join(valid_loader.dataset.patch_root, this_image), output['boxes'])
-                        label_image = to_pil_image(labeled)
-                        label_image.save(os.path.join(save_dir, 'labeled_images', this_image))
+                        #The function "label_on_image" is now defunct. Instead, use "label_volume" on a .mov file.
+                        #labeled = label_on_image(os.path.join(valid_loader.dataset.patch_root, this_image), output['boxes'])
+                        #label_image = to_pil_image(labeled)
+                        #label_image.save(os.path.join(save_dir, 'labeled_images', this_image))
                     
 
                     # if 'masks' in output:
@@ -195,141 +195,218 @@ def get_metrics(boxes: List[Tensor], scores: List[Tensor], targets: List[Tensor]
     mean_iou = iou.mean()
     return average_precisions, mean_iou
 
-def label_on_image(image_file, boxes):
-
-    maxBubblesPerFrame = 150                    # length of bubble tracking array
-    cols = 7                                    # width of bubble tracking array (number of datapoints for each bubble)
-    curr = np.zeros((maxBubblesPerFrame, cols)) # current frame bubble tracking array
-    prev = np.zeros((maxBubblesPerFrame, cols)) # previous frame bubble tracking array
-
-    lifetime_numBubbles = 1                     # counter for how many unique bubbles have been identified
-
-    precision_ID = 10                           # number of pixels the center of a bubble can drift from one frame to the next without triggering a new instance
-    precision_coalesce = 10                     # If a bubble dissapears, all bubbles within this number of pixels are assumed to have participated in coalesence
-    limit_nucleation = 23                       # If a newly identified bubble has a diameter greater than this number (in pixels) its growth is ignored (must not be a
-                                                # newly nucleated bubble, rather a large bubble that was lost in the previous frame and then re-recognized)
-
-    counter = 0
-
-    image = skimage.io.imread(image_file)
-
-    N = boxes.shape[0]
-    if not N:
-        print("\n*** No instances to display *** \n")
-
-    for i in range(N):
-        if np.size(boxes[i]) == 0:
-            # Skip this instance. Has no bbox. Likely lost in image cropping.
+def check_coalesce(center,radius,prev_circles,curr_circles,min_c=10):
+    '''
+    Performs a check of coalescence of bubbles between frames. Definition of coalescence:
+    -Take two bubbles A and B at time (T-1) with radii r_A, r_B, respectively.
+    -The two bubbles must be at a distance (r_A+r_B+min_c), otherwise coalescence not considered
+    -At time step T, only one of bubbles A and B remain, and the radius of A has increased substantially.
+    Parameters
+    ----------
+    center: list
+        coordinates (x,y) for the center of a circle
+    radius: float (or int)
+        radius of circle in question
+    prev_circles: dict
+        dictionary containing all relevant info of past circles
+    curr_circles: dict
+        dictionary containing all relevant info of current circles
+    min_c: int
+        minimum distance between neighboring circles at which coalescence is deemed possible
+    Returns
+    -------
+    is_coalesced: bool
+        boolean where if True coalesence occurred, else False
+    '''
+    past_centers = np.array([np.array(prev_circles[i]['center']) for i in sorted(prev_circles.keys())])
+    all_new_centers = np.array([np.array(curr_circles[i]['center']) for i in sorted(curr_circles.keys())])
+    past_ind = np.argmin(np.sum((past_centers-np.array(center))**2,axis=1)**0.5)
+    past_match = prev_circles[past_ind]
+    poss_coalesce_inds = []
+    for i in sorted(prev_circles.keys()):
+        if i==past_ind: #Skip duplicates for past bubble frame
             continue
-        x1, y1, x2, y2 = boxes[i]   # corners of bounding box. The accuracy of this algorithm could be greatly improved by  interpreting the masks directly as opposed to using the bounding box
+        poss_center = np.array(prev_circles[i]['center'])
+        curr_center = past_match['center']
+        if np.sum((poss_center-curr_center)**2)**0.5 < prev_circles[i]['radius']+past_match['radius']+min_c:
+            poss_coalesce_inds.append(i) #Circles are close enough that they have potential to merge
+    #For potential merge candidates, did any of them actually coalesce? If no match, then coalescenced occurred
+    for i in poss_coalesce_inds:
+        poss_center = np.array(prev_circles[i]['center'])
+        new_center_dists = np.sum((all_new_centers-np.array(poss_center))**2,axis=1)**0.5
+        if min(new_center_dists)>10: #No match to previous frame, so coalescence must have occurred
+            return True #Coalescence
+    return False #No coalescence
 
-        D = int((x2+y2-x1-y1)/2)    # width of bounding box == diameter of bubble
-        center_x = int((x2+x1)/2)
-        center_y = int((y2+y1)/2)
-        match_plusminus = 0
 
-        image = np.array(image)
-        if D < 200 and i < maxBubblesPerFrame:    #bubble found!!
-            #Image editing
-            image = image.astype(np.uint8)
-        
-        
-# instance growth tracking: calculating the size +- for each bubble
-            # bubble tracking array
-            # | 0  | 1         | 2         | 3         | 4    | 5          | 6       |
-            # | ID | center X  | center Y  | Diameter  | New? | Coalesced? | Size +- |
-            curr[i, 1] = center_x
-            curr[i, 2] = center_y
-            curr[i, 3] = D
-
-            # go through the previous bubble tracking array and see if the current instance existed in the previous frame
-            foundmatch = 0
-            match_row = 0
-            for j in range(maxBubblesPerFrame):
-                if (np.abs(prev[j, 1]-center_x) + np.abs(prev[j, 2] - center_y)) < precision_ID:    # match based on center position
-                    foundmatch = 1
-                    curr[i, 0] = prev[j, 0] #assign id's
-                    match_row = j
-                    match_plusminus = prev[j, 6]
-                    break
-
-            # calculate Size +-
-            if not foundmatch: #newly identified bubble!
-                curr[i, 4] = 1 #set new flag
-                curr[i, 0] = lifetime_numBubbles
-                lifetime_numBubbles = lifetime_numBubbles + 1
-                if (D < limit_nucleation): # check if "new" bubble is small enough to actually be a new bubble as opposed to a lost + refound bubble.
-                    curr[i, 6] = 3.14/6*D*D*D # volume of a sphere
-                else:
-                    curr[i, 6] = 0 #change its +- to zero
-                    curr[i, 4] = 0 #remove its fresh tag
-
-            else: #not a new bubble
-                curr[i, 4] = 0
-                curr[i, 6] = 3.14/6*(D*D*D - prev[match_row, 3]*prev[match_row, 3]*prev[match_row, 3])      # incremental growth from the previous frame
-                    
+def compare_bubbles(center,radius,prev_circles,min_dist=10):
+    '''
+    Takes an existing bubble in a frame, determines whether the bubble is new, assessess volume change
+    Parameters
+    ----------
+    center: list
+        coordinates (x,y) for the center of a circle
+    radius: float (or int)
+        radius of circle in question
+    prev_circles: dict
+        dictionary containing all relevant info of past circles
+    min_dist: int
+        the minimum distance between circle centers at which one judges the formation of a new bubble
+    Returns
+    -------
+    new: bool
+        boolean stating whether a bubble is new in the current frame (True) or recurring (False)
+    coalesced: bool (or None)
+        boolean stating whether a bubble coalesced between frames (None if undetermined)
+    volume_change: float (or None)
+        change in volume of a bubble between slides (units are technically in px**3)
+    old_id: int (or None)
+        if bubble is not new, then keep the same old id between frames
+    '''
+    all_centers = np.array([np.array(prev_circles[i]['center']) for i in sorted(prev_circles.keys())])
+    if len(all_centers)==0:
+        new=True; coalesced=False; volume_change=None; old_id=None
+        return new, coalesced, volume_change, old_id
+    #Find matching circle based on min_dist parameter
+    center_dists = np.sum((all_centers-np.array(center))**2,axis=1)**0.5
+    if min(center_dists)<=10: #match located in previous frame
+        matching_circle_data = prev_circles[np.argmin(center_dists)]
+        new=False
+        volume_change = 2.*np.pi / 3 * (radius**3 - matching_circle_data['radius']**3)
+        if volume_change<=10: #Can edit threshold on what sort of volume change warrants coalescence
+            coalesced=False
+            return new, coalesced, volume_change, matching_circle_data['id']
         else:
-            print("ignored (too big or too many)!")
+            coalesced=None
+            return new, coalesced, volume_change, matching_circle_data['id'] 
+    else: #No match found, therefore circle is new
+        new=True; coalesced=False; volume_change=None; old_id=None
+        return new, coalesced, volume_change, old_id
 
-# Coalesence invalidation: if a bubble participates in a coalesence event, it should not contribute to the Size +-
-    for p in range(maxBubblesPerFrame):
-        # check if each bubble from prev still exists in curr
-        coalesce = 1
-        for c in range(maxBubblesPerFrame):
-            if(prev[p, 0] == curr[c, 0]):   #bubble still exists
-                coalesce = 0
-                break
+def redraw_fig(frame,bubble_dict):
+    '''
+    Uses OpenCV functions on an existing frame to add circles based on category as well as id numbers
+    Parameters
+    ----------
+    frame: ndarray
+        image array of shape (x_len, y_len, 3) containing bubbles
+    bubble_dict: dict
+        dictionary containing all relevant info of current circles
+    Returns
+    -------
+    edit_frame: ndarray
+        image array after cv2 has inserted circles and text at circle locations
+    '''
+    change_cutoff=50
+    edit_frame = frame.copy()
+    color_dict = {'new':(224, 230, 73),'coalesced':(96, 66, 245),\
+                  'growing':(50, 168, 82),'shrinking':(150, 0, 0),'unchanged':(179, 247, 255)}
+    for b in bubble_dict.keys():
+        bubble = bubble_dict[b]
+        color=None
+        if bubble['new']: color = color_dict['new']
+        if bubble['coalesced']: color = color_dict['coalesced']
+        if color is None: #New and coalesced categories take precedent over others
+            if bubble['volume_change'] > change_cutoff: color = color_dict['growing']
+            elif bubble['volume_change'] < -change_cutoff: color = color_dict['shrinking']
+            else: color = color_dict['unchanged']
+        cv2.circle(edit_frame, (bubble['center_x'], bubble['center_y']), int(bubble['radius']), color, 3)
+        cv2.putText(edit_frame, str(bubble['id']), (bubble['center_x']-9, bubble['center_y']+7),\
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+    return edit_frame
 
-        if(coalesce):
-            for c2 in range(maxBubblesPerFrame):
-                dx = np.abs(prev[p, 1]-curr[c2, 1])
-                dy = np.abs(prev[p, 2]-curr[c2, 2])
-                dd = np.sqrt(dx*dx + dy*dy)             # distance between bubble centers
-                
-                avg_D = int((prev[p, 3] + curr[c2, 3])/2)
-                
-                if (dd - avg_D < precision_coalesce):   # If a bubble is within precision_coalesence of the dissapeared bubble, it should not contribute to Size +-
-                    curr[c2, 5] = 1   # coalesence flag
-#                   curr[c2, 6] = 0  # zeroth order
-                    curr[c2, 6] = match_plusminus  # first order (take the plus/minus from the last frame)
+def label_volume(movie_file, all_boxes, write_frame_csv=False, write_overall=False, save_video=False):
+    '''
+    Takes in a movie file and all specified bounding boxes, returns descriptor files and edited movie.
+    Parameters
+    ----------
+    movie_file: str
+        path to the movie file to be parsed
+    all_boxes: list
+        list of bounding box locations at each time step
+    write_frame_csv: bool
+        trigger to write csv files at each frame that describe all bubbles
+    write_overall: bool
+        trigger to write a csv file that describes the overall movement and process of the bubbles
+    save_video: bool
+        trigger to write a separate video file with edited frames with circles and ids per bubble
+    Returns
+    -------
+    overall_df: pd.DataFrame
+        dataframe describing the full process of bubbles changing between frames
+    '''
+    capture = cv2.VideoCapture(movie_file)
+    new_vid = None
+    success = True
+    prev_circles = {}
+    total_volume_li = []
+    num_bubbles_li = []
+    max_id_num = 0
+    frame = 0
+    while success: #Iterating over initial image frames, writing new frames to new video
+        success,curr_frame = capture.read()
+        if new_vid is None and save_video: #Open new video to begin writing, if desired
+            fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+            new_vid = cv2.VideoWriter('test_vid_out.mov',fourcc,60,\
+                                       (int(capture.get(3)),int(capture.get(4))))
+        if not success:
+            break
+        curr_boxes = all_boxes[frame]
+        curr_circles = {} #Tracking dict for bubbles in current frame
+        circle_counter = 0
+        vol_in_frame = 0
+        coalesce_check = []
+        for box in curr_boxes:
+            curr_circles[circle_counter]={} #Tracking dict for current bubble
+            center = [0.5*(box[0]+box[2]),0.5*(box[1]+box[3])] #Center averaging
+            curr_circles[circle_counter]['center'] = center
+            curr_circles[circle_counter]['center_x'] = int(center[0])
+            curr_circles[circle_counter]['center_y'] = int(center[1])
+            radius = 0.5*(abs(box[0]-center[0]) + abs(box[1]-center[1])) #Radius averaging
+            curr_circles[circle_counter]['radius'] = radius
+            curr_circles[circle_counter]['diameter'] = int(radius*2)
+            new, coalesced, volume_change, old_id = compare_bubbles(center,radius,prev_circles)
+            if new:
+                curr_circles[circle_counter]['id']=max_id_num
+                max_id_num+=1
+            else:
+                curr_circles[circle_counter]['id']=old_id
+            curr_circles[circle_counter]['new']=new
+            curr_circles[circle_counter]['coalesced']=coalesced
+            curr_circles[circle_counter]['volume_change']=volume_change
+            vol_in_frame+=2.*np.pi/3. * radius**3 #Sum all circle volumes
+            if coalesced is None:
+                coalesce_check.append(circle_counter)
+            circle_counter+=1
+        #Check after for possible coalescences
+        for i in coalesce_check:
+            curr_circles[i]['coalesced']=check_coalesce(curr_circles[i]['center'],\
+                                                        curr_circles[i]['radius'],\
+                                                        curr_circles,prev_circles)
+        total_volume_li.append(vol_in_frame) #Keep a list of all recorded volumes
+        num_bubbles_li.append(int(len(curr_circles)))
+        if len(curr_circles)>0:
+            curr_df = pd.DataFrame.from_dict(curr_circles).T.drop(columns=['center',\
+                                                                'radius']).sort_values(by='id')
+        if save_video: #Write edited frame to new video
+            new_vid.write(redraw_fig(curr_frame,curr_circles))
+        if write_frame_csv:
+            curr_df.to_csv(f'frame_{frame}.csv',index=False)
+        prev_circles = curr_circles
+        frame+=1
+    #Release videos when runs are done
+    if save_video: new_vid.release()
+    capture.release()
+    overall_df = pd.DataFrame(np.array([total_volume_li,np.cumsum(total_volume_li).tolist(),\
+                                        np.diff(total_volume_li,prepend=0.),num_bubbles_li]).T,\
+                              columns=['frame_bubble_V','diff_bubble_V','total_bubble_V','num_bubbles'])
+    if write_overall:
+        overall_df.to_csv('overall_results.csv',index=False)
+    return overall_df
 
+#Test use, if desired
+#_ = label_volume('test_bubbles.mov',all_boxes,write_overall=True,save_video=True)
 
-# summing all the Size +- and assigning colors
-    sum = 0
-    for s in range(maxBubblesPerFrame):
-        sum = sum + curr[s, 6]
-
-        color = (255, 255, 255)
-        if (curr[s, 4]):  #New bubble!
-            color = (224, 230, 73)
-        elif (curr[s, 5]):  #coalesced bubble
-            color = (96, 66, 245)
-        elif (curr[s, 6] > 0): #growing bubble
-            color = (50, 168, 82)
-        elif(curr[s, 6] == 0): # size unchanged
-            color = (179, 247, 255)
-        else:   #shrinking bubble
-            color = (150, 0, 0)
-
-        # draw circle and write ID on frame
-        cv2.putText(image, str(int(curr[s, 0])), (int(curr[s, 1])-9, int(curr[s, 2])+7), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
-        cv2.circle(image, (int(curr[s, 1]), int(curr[s, 2])), int(curr[s, 3]/2), color, 3)
-
-    with open("res.csv", 'a') as f: # log the total size+- for the counter-th frame
-        f.write(str(counter) + ", " + str(sum))
-        f.write("\n")
-
-
-# Resetting
-    if not os.path.isdir("./ID_table/"):
-        os.makedirs("./ID_table")
-    np.savetxt("./ID_table/" + str(counter) + ".csv", curr, delimiter=",")  # save all data to csv
-    prev = curr
-    curr = np.zeros((maxBubblesPerFrame, cols))
-    counter = counter + 1
-# end Resetting
-
-    return image
 
 def run_apply(args):
     test_dir = os.path.join(args.root, args.name, args.test_dir)

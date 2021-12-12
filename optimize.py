12ce512ab8e13d7813510fb6f096204e82b43006
NathanPruyne <nathan.pruyne@gmail.com>
2021-12-12 00:51:27 -0600

    Fix quick bug on patch sizing

--- optimize.py
+++ optimize.py
@@ -85,7 +85,7 @@ def pick_patch_size(args: SimpleNamespace):
         raise ValueError('Min patch size is larger than max patch size')
     def f(spec):
         min_valid_64 = args.min_patch_size // 64 + 1
-        max_valid_32 = args.max_patch_size // 64
+        max_valid_64 = args.max_patch_size // 64
         return nprd.randint(min_valid_64, max_valid_64 + 1) * 64
     return f
 

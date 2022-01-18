99d68e2c2b6b63c762dc0a52cc939f74d6573a3d
Jim James <github@jimjam.es>
2022-01-18 15:31:44 -0500

    Write json files for generated data, reject bubbles with too much overlap

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,17 +1,19 @@
 import json
 import os
-import shutil
 import random
+import shutil
 
+import cv2
 import numpy as np
 import torchvision.transforms as T
 from PIL import Image
 from torchvision.transforms.functional import pil_to_tensor
+from tqdm import tqdm
 
 from utils import get_circle_coords
 
 
-def extract_bubbles(image_root, json_root, output_dir):
+def extract_bubbles(image_root, json_root, output_dir, trim=2):
     i = 0
     if os.path.isdir(output_dir):
         shutil.rmtree(output_dir)
@@ -43,7 +45,7 @@ def extract_bubbles(image_root, json_root, output_dir):
                 radius_y = bubble[3]
 
                 # Generate points for alpha channel
-                rr, cc = get_circle_coords(center_y, center_x, radius_y, radius_x, height, width)
+                rr, cc = get_circle_coords(center_y, center_x, radius_y - trim, radius_x - trim, height, width)
                 y_min = np.min(rr)
                 y_max = np.max(rr)
                 x_min = np.min(cc)
@@ -60,17 +62,17 @@ def extract_bubbles(image_root, json_root, output_dir):
                     i += 1
 
 
-def augment_bubble(bubble: Image.Image):
-    bubble_tensor = pil_to_tensor(bubble)
+def augment_image(image: Image.Image):
+    image_tensor = pil_to_tensor(image)
     transform = T.Compose([
         T.ColorJitter(0.25, 0.25, 0.25),
         T.RandomApply([T.GaussianBlur(3, 0.1)]),
         T.RandomAdjustSharpness(0.2),
     ])
-    bubble_tensor[:3, :, :] = transform(bubble_tensor[:3, :, :])
-    bubble_tensor = bubble_tensor.permute((1, 2, 0)).numpy()
-    bubble = Image.fromarray(bubble_tensor)
-    return bubble
+    image_tensor[:3, :, :] = transform(image_tensor[:3, :, :])
+    image_tensor = image_tensor.permute((1, 2, 0)).numpy()
+    image = Image.fromarray(image_tensor)
+    return image
 
 
 def generate_data(root: str, num_samples: int, min_bubble: int, max_bubble: int, width_padding: int = 200, height_padding: int = 200):
@@ -81,26 +83,51 @@ def generate_data(root: str, num_samples: int, min_bubble: int, max_bubble: int,
     output_dir = os.path.join(root, 'output')
     if os.path.isdir(output_dir):
         shutil.rmtree(output_dir)
-    os.mkdir(output_dir)
+    os.makedirs(os.path.join(output_dir, 'images'))
+    os.mkdir(os.path.join(output_dir, 'json'))
     extract_bubbles(image_root, json_root, bubble_root)
     num_bubbles = len(os.listdir(bubble_root))
-    for sample in range(num_samples):
+    for sample in tqdm(range(num_samples)):
+        output_json = {'annotations': []}
         background_path = os.path.join(background_root, random.choice(os.listdir(background_root)))
         background = Image.open(background_path).convert('RGBA')
+        background = augment_image(background)
         num_bubbles = np.random.randint(min_bubble, max_bubble + 1)
+        mask = np.zeros((background.height, background.width), dtype=np.uint8)
         for _ in range(num_bubbles):
             bubble_path = os.path.join(bubble_root, random.choice(os.listdir(bubble_root)))
             bubble_image = Image.open(bubble_path).convert('RGBA')
-            bubble_image = augment_bubble(bubble_image)
+            bubble_image = augment_image(bubble_image)
             x = np.random.randint(width_padding, background.width - bubble_image.width + 1 - width_padding)
             y = np.random.randint(height_padding, background.height - bubble_image.height + 1 - height_padding)
+            mask_patch = mask[y: y + bubble_image.height, x: x + bubble_image.width]
+            bubble_mask = np.asarray(bubble_image)[:, :, 3] // 255
+            overlap_percent = np.sum(mask_patch & bubble_mask) / np.sum(bubble_mask)
+            if overlap_percent > 0.55:
+                continue
+            bubble = {
+                'ellipse': {
+                    'center': {
+                        'x': bubble_image.width/2 + x,
+                        'y': bubble_image.height/2 + y
+                    },
+                    'radius': {
+                        'x': bubble_image.width/2,
+                        'y': bubble_image.height/2
+                    }
+                }
+            }
+            output_json['annotations'].append(bubble)
             background.alpha_composite(bubble_image, (x, y))
+            mask[y: y + bubble_image.height, x: x + bubble_image.width] |= bubble_mask
 
         background_np = np.asarray(background)
         background_np[:, :, 3] = 255
         background = Image.fromarray(background_np)
-        background.save(os.path.join(output_dir, '%d.png' % sample))
+        background.save(os.path.join(output_dir, 'images', '%d.png' % sample))
+        with open(os.path.join(output_dir, 'json', '%d.json' % sample), 'w') as f:
+            json.dump(output_json, f)
 
 
 if __name__ == '__main__':
-    generate_data('data/data_generation/pristine', 5, 10, 50)
+    generate_data('data/data_generation/pristine', 200, 10, 60)

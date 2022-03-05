78d7778b3cca8a4f47002e05dece23f97f2048f4
Aristana Scourtas <ascourtas@gmail.com>
2022-03-04 23:23:51 -0500

    add basic arg parsing:

--- grab_stills.py
+++ grab_stills.py
@@ -8,32 +8,44 @@ Code snippets borrowed from https://www.geeksforgeeks.org/extract-images-from-vi
 last updated 3/4/22 by Aristana Scourtas
 """
 
+from argparse import ArgumentParser
 import cv2
 import math
 import os
 
-video_name = "IrO2_DSC_0106"
-dest_folder = "./still_data"
-source_video_path = "/Users/aristanascourtas/Documents/Work/bubble misc/Test Videos-2-28-2022/DSC_0106.MOV"
 
-cam = cv2.VideoCapture(source_video_path)
-# frame
+parser = ArgumentParser(description="Utility script to grab still images from videos, labeling them with timestamps "
+                                    "in microseconds. Intended for grabbing stills from catalysis images for labeling "
+                                    "(ie annotating).")
+parser.add_argument("--short_name", dest="video_name", type=str, required=True,
+                    help="The shortname for the video to process, which will be written into the still image "
+                         "filenames. Ex: IrO2_DSC_0106")
+parser.add_argument("--source_path", dest="source_video_path", type=str, required=True,
+                    help="The full filepath to the source video to process.")
+parser.add_argument("--dest", dest="dest_folder", type=str, default="./still_data",
+                    help="The path of the folder to which the still images will be written. Default is "
+                         "'./still_data'")
+parser.add_argument("--save_rate", dest="save_rate", type=int, default=30,
+                    help="The number of seconds between each frame to save. Default is 30. "
+                         "Ex: 30 would mean saving 1 frame every 30s")
+
+args = parser.parse_args()
+
+cam = cv2.VideoCapture(args.source_video_path)
+# frame counter
 currentframe = 0
 
 # get video FPS (e.g. ~60 fps)
 fps = cam.get(cv2.CAP_PROP_FPS)
 
-# the seconds between frame saves
-save_rate = 30
-
 # calculate the number of n frames in each save_rate interval
-n_frame_to_grab = save_rate * math.ceil(fps)
+n_frame_to_grab = args.save_rate * math.ceil(fps)
 
 
 try:
     # create a local folder to save the data
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
+    if not os.path.exists(args.dest_folder):
+        os.makedirs(args.dest_folder)
 except OSError:
     print('Error: Creating directory of data')
 
@@ -47,8 +59,8 @@ while True:
     if currentframe % n_frame_to_grab == 0.0:
         # if video is still left continue writing images
         if ret:
-            filename = f"{video_name}_{str(timestamp)}.jpg"
-            dest_path = os.path.join(dest_folder, filename)
+            filename = f"{args.video_name}_{str(timestamp)}.jpg"
+            dest_path = os.path.join(args.dest_folder, filename)
             print('Writing ' + dest_path)
 
             # write the extracted images to disk

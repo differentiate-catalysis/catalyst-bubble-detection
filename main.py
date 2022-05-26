4a3bb770cf5e9153eae9bb65d998d830875ae3c9
Jim James <github@jimjam.es>
2022-05-25 23:30:54 -0500

    Allow for inferencing an entire directory of videos, add more comments to main, marginally speed up evaluate on videos

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import argparse
 import json
 import os
 import shutil
+from pathlib import Path
 from types import SimpleNamespace
 from typing import List
 
 import torch.multiprocessing as mp
 
-from conversions import gen_label_images, gen_targets, apply_transforms
+from arg_checks import check_args
+from conversions import apply_transforms, gen_label_images, gen_targets
 from evaluate import run_apply, run_metrics
 from MatCNN.evaluate import run_apply as mat_run_apply
 from MatCNN.evaluate import run_metrics as mat_run_metrics
@@ -20,7 +22,6 @@ from models import model_keys as rcnn_models
 from optimize import optimize
 from train import train_model
 from utils import gen_args
-from arg_checks import check_args
 
 
 def main(args: SimpleNamespace, changed: List[str], explicit: List[str]):
@@ -63,7 +64,26 @@ def main(args: SimpleNamespace, changed: List[str], explicit: List[str]):
             run_metrics(args, loss=loss)
             valid_mode = True
         if 'apply' in modes:
-            run_apply(args)
+            if args.video_dir is not None:
+                videos = []
+                video_formats = ['mp4', 'mov']
+                for format in video_formats:
+                    videos.extend(list(Path(args.video_dir).rglob('*.%s' % format.lower())))
+                    videos.extend(list(Path(args.video_dir).rglob('*.%s' % format.upper())))
+                videos = set(videos)
+                print('Found %d videos.' % len(videos))
+                root = args.root
+                for video in videos:
+                    video = str(video)
+                    args.video = video
+                    basename = os.path.basename(args.video)
+                    dot_index = basename.rfind('.')
+                    if dot_index != -1:
+                        basename = basename[: dot_index]
+                    args.root = os.path.join(root, basename)
+                    run_apply(args)
+            else:
+                run_apply(args)
             valid_mode = True
         if 'metrics' in modes:
             run_metrics(args)
@@ -110,83 +130,6 @@ def main(args: SimpleNamespace, changed: List[str], explicit: List[str]):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--root', type=str, help='Root directory for the dataset')
-    parser.add_argument('--lr', type=float, help='Learning rate for the optimizer')
-    parser.add_argument('--batch_size', type=int, help='Batch size')
-    parser.add_argument('--epoch', type=int, help='Number of epochs to train for')
-    parser.add_argument('--gpu', type=int, help='Device ordinal for CUDA GPU')
-    parser.add_argument('--amp', action='store_true', help='Use mixed precision')
-    parser.add_argument('--opt', type=str, help='Optimizer to train with. Either \'adam\' or \'sgd\'')
-    parser.add_argument('--momentum', type=float, help='Momentum for SGD')
-    parser.add_argument('--aug', help='Whether or not to augment inputs', action='store_true')
-    parser.add_argument('--test_dir', type=str, help='Directory to test model on')
-    parser.add_argument('--save', type=str, help='Directory to write saved model weights to. Subdirectories will be created for HPO runs')
-    parser.add_argument('--mode', type=str, nargs='+', help='Mode to run. Available modes include \'train\', \'apply\', \'evaluate\', and \'optimize\'')
-    parser.add_argument('--mp', action='store_true', help='Whether or not to use multiprocessing')
-    parser.add_argument('--nodes', type=int, help='Number of nodes for multiprocessing')
-    parser.add_argument('--nr', type=int, help='Index of the current node\'s first rank')
-    parser.add_argument('--transforms', type=str, nargs='*', help='Which augmentations to use. Choose some combination of \'vertical_flip\', \'horizontal_flip\', and \'rotation\'')
-    parser.add_argument('--model', type=str, help='Which model architecture to use. Available models include \'mask_rcnn\' and \'faster_rcnn\'')
-    parser.add_argument('--config', type=str, help='Location of a full configuration file')
-    parser.add_argument('--json_dir', type=str, help='Directory containing JSON files')
-    parser.add_argument('--num_images', type=int, help='Number of images to train on')
-    parser.add_argument('--checkpoint', type=str, help='Directory to a pth file to load')
-    parser.add_argument('--no-prompt', dest='prompt', action='store_false', help='Skip prompt for checkpoint loading')
-    parser.add_argument('--sampling_models_hpo', type=str, nargs='+', help='Models to sample from in HPO')
-    parser.add_argument('--min_lr_hpo', type=float, help='Minimum learning rate for HPO')
-    parser.add_argument('--max_lr_hpo', type=float, help='Maximum learning rate for HPO')
-    parser.add_argument('--min_epochs_hpo', type=int, help='Minimum number of epochs for HPO')
-    parser.add_argument('--max_epochs_hpo', type=int, help='Maximum number of epochs for HPO')
-    parser.add_argument('--min_momentum_hpo', type=float, help='Minimum momentum for HPO')
-    parser.add_argument('--max_momentum_hpo', type=float, help='Maximum momentum for HPO')
-    parser.add_argument('--max_patch_size_hpo', type=int, help='Maximum patch size for HPO')
-    parser.add_argument('--min_patch_size_hpo', type=int, help='Minimum patch size for HPO')
-    parser.add_argument('--max_batch_size_hpo', type=int, help='Maximum batch size for HPO')
-    parser.add_argument('--min_batch_size_hpo', type=int, help='Minimum batch size for HPO')
-    parser.add_argument('--max_gamma_hpo', type=float, help='Max gamma value for HPO')
-    parser.add_argument('--min_gamma_hpo', type=float, help='Min gamma value for HPO')
-    parser.add_argument('--num_samples_hpo', type=int, help='Number of trials to run for HPO')
-    parser.add_argument('--optimizers_hpo', type=str, nargs='+', help='Optimizers to sample from in HPO')
-    parser.add_argument('--resume_hpo', action='store_true', help='Whether or not to resume an HPO trial')
-    parser.add_argument('--jobs', type=int, help='Number of cores to use for parsl')
-    parser.add_argument('--data_workers', type=int, help='Number of processes to run for dataloading')
-    parser.add_argument('--name', type=str, help='Name of the model')
-    parser.add_argument('--graph', action='store_true', help='Whether or not to save a plot of the results')
-    parser.add_argument('--run_dir', type=str, help='Directory to use for parsl')
-    parser.add_argument('--loss', type=str, help='Loss function to use.')
-    parser.add_argument('--version', type=str, help='Version of the network (for MatCNN compatibility)')
-    parser.add_argument('--patience', type=int, help='Use patience after how many epochs')
-    parser.add_argument('--image_size', type=int, nargs=2, help='Size of images processed as x y (necessary for evaluate re-stitching)')
-    parser.add_argument('--num_patches', type=int, nargs='+', help='Number of patches each slice is split into. Set to 0 for automatic choice based on patch and image size.')
-    parser.add_argument('--overlap_size', type=int, help='Width/height of the overlap between each patch')
-    parser.add_argument('--slices', type=int, help='Number of slices to take at a time')
-    parser.add_argument('--no-overlay', help='On evaluate, create full stitched images rather than overlays (if applicable)', dest='overlay', action='store_false')
-    parser.add_argument('--data-split', type=float, nargs=3, help="3 floats for test validation train split of data in image2npy (in order test, validation, train)", dest='split')
-    parser.add_argument('--collect', action='store_true', help='Whether or not to manually garbage collect each input')
-    parser.add_argument('--patching_mode', type=str, help="Mode with which to patch images. Either \"grid\" or \"random\"")
-    parser.add_argument('--clear_predictions', action='store_true', help='Clear prediction patches during evaluate')
-    parser.add_argument('--tar', action='store_true', help='Tar output when done')
-    parser.add_argument('--sampling_slices_hpo', type=int, nargs='+', help='Numbers of slices to use for models in HPO')
-    parser.add_argument('--sampling_patching_modes_hpo', type=str, nargs='+', help='Patching modes to sample with in HPO')
-    parser.add_argument('--min_overlap_size_hpo', type=int, help='Smallest size of overlap in HPO')
-    parser.add_argument('--max_overlap_size_hpo', type=int, help='Maximum size of overlap in HPO')
-    parser.add_argument('--mask', type=str, help='Mask to apply when getting metrics')
-    parser.add_argument('--losses', type=str, nargs='+', help='Losses to sample from in HPO')
-    parser.add_argument('--train_set', type=str, help='Dataset to use only as train')
-    parser.add_argument('--test_set', type=str, help='Dataset to use only as test')
-    parser.add_argument('--val_set', type=str, help='Dataset to use only as validation')
-    parser.add_argument('--blocks', type=int, help='Number of encoding blocks to use in each architecture')
-    parser.add_argument('--patch_size', type=int, help='Size of patches to use for target generation. Set to -1 to disable patching')
-    parser.add_argument('--data_split', type=float, nargs=3, help="3 floats for test validation train split of data in image2npy (in order test, validation, train)", dest='split')
-    parser.add_argument('--gamma', type=float, help='Amount to decrease LR by every 3 epochs')
-    parser.add_argument('--imagenet_stats', action='store_true', help='Use ImageNet stats instead of dataset stats for normalization')
-    parser.add_argument('--stats_file', type=str, help='JSON file to read stats from')
-    parser.add_argument('--video', type=str, help='Video file to perform inference on')
-    parser.add_argument('--simclr_checkpoint', type=str, help='Weights for SimCLR pre-trained ResNet')
-    parser.add_argument('--augment_out', type=str, help='Output directory for augment mode')
-
-    args = parser.parse_args()
     defaults = {
         'root': 'data/bubbles',
         'gpu': -1,
@@ -259,10 +202,89 @@ if __name__ == '__main__':
         'imagenet_stats': False,
         'stats_file': None,
         'video': None,
+        'video_dir': None,
         'simclr_checkpoint': None,
         'augment_out': 'data/augment',
         'data_workers': 0,
     }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type=str, help='Root directory for the dataset. Default: %s' % defaults['root'])
+    parser.add_argument('--lr', type=float, help='Learning rate for the optimizer. Default: %s' % defaults['lr'])
+    parser.add_argument('--batch_size', type=int, help='Batch size. Default: %s' % defaults['batch_size'])
+    parser.add_argument('--epoch', type=int, help='Number of epochs to train for. Default: %s' % defaults['epoch'])
+    parser.add_argument('--gpu', type=int, help='Device ordinal for CUDA GPU. Set to -1 to run on CPU only. Default: %s' % defaults['gpu'])
+    parser.add_argument('--amp', action='store_true', help='Use mixed precision (reduces VRAM requirements). Default: %s' % defaults['amp'])
+    parser.add_argument('--opt', type=str, help='Optimizer to train with. Either \'adam\' or \'sgd\'. Default: %s' % defaults['opt'])
+    parser.add_argument('--momentum', type=float, help='Momentum for SGD. Default: %s' % defaults['momentum'])
+    parser.add_argument('--aug', help='Whether or not to augment inputs', action='store_true') #TODO: remove
+    parser.add_argument('--test_dir', type=str, help='Directory to test model on. Default: %s' % defaults['test_dir'])
+    parser.add_argument('--save', type=str, help='Directory to write saved model weights to. Subdirectories will be created for HPO runs. Default: %s' % defaults['save'])
+    parser.add_argument('--mode', type=str, nargs='+', help='Mode to run. Available modes include \'train\', \'apply\', \'evaluate\', and \'optimize\'')
+    parser.add_argument('--mp', action='store_true', help='Whether or not to use multiprocessing. Only use if you are training with multiple GPUs or multiple nodes. Default: %s' % defaults['mp'])
+    parser.add_argument('--nodes', type=int, help='Number of nodes for multiprocessing. Only necessary if mp is set.')
+    parser.add_argument('--nr', type=int, help='Index of the current node\'s first rank. Only necessary if mp is set.')
+    parser.add_argument('--transforms', type=str, nargs='*', help='Which augmentations to use. Choose some combination of \'vertical_flip\', \'horizontal_flip\', and \'rotation\'. Default: %s' % defaults['transforms'])
+    parser.add_argument('--model', type=str, help='Which model architecture to use. Available models include \'mask_rcnn\', \'faster_rcnn\', \'retina_net\', and \'simclr_faster_rcnn\'. Default: %s' % defaults['model'])
+    parser.add_argument('--config', type=str, help='Location of a full configuration file. This contains options for a given run.')
+    parser.add_argument('--json_dir', type=str, help='Directory containing several JSON configuration files.')
+    parser.add_argument('--num_images', type=int, help='Number of images to train on') # idt this is used tbh
+    parser.add_argument('--checkpoint', type=str, help='Filepath to a set of saved weights (pth file) to load')
+    parser.add_argument('--no-prompt', dest='prompt', action='store_false', help='Skip prompt for checkpoint loading. Default: %s' % (not defaults['prompt']))
+    parser.add_argument('--sampling_models_hpo', type=str, nargs='+', help='Model architectures to sample from in HPO. Default: %s' % defaults['sampling_models_hpo'])
+    parser.add_argument('--min_lr_hpo', type=float, help='Minimum learning rate for HPO')
+    parser.add_argument('--max_lr_hpo', type=float, help='Maximum learning rate for HPO')
+    parser.add_argument('--min_epochs_hpo', type=int, help='Minimum number of epochs for HPO')
+    parser.add_argument('--max_epochs_hpo', type=int, help='Maximum number of epochs for HPO')
+    parser.add_argument('--min_momentum_hpo', type=float, help='Minimum momentum for HPO')
+    parser.add_argument('--max_momentum_hpo', type=float, help='Maximum momentum for HPO')
+    parser.add_argument('--max_patch_size_hpo', type=int, help='Maximum patch size for HPO')
+    parser.add_argument('--min_patch_size_hpo', type=int, help='Minimum patch size for HPO')
+    parser.add_argument('--max_batch_size_hpo', type=int, help='Maximum batch size for HPO')
+    parser.add_argument('--min_batch_size_hpo', type=int, help='Minimum batch size for HPO')
+    parser.add_argument('--max_gamma_hpo', type=float, help='Max gamma value for HPO')
+    parser.add_argument('--min_gamma_hpo', type=float, help='Min gamma value for HPO')
+    parser.add_argument('--num_samples_hpo', type=int, help='Number of trials to run for HPO')
+    parser.add_argument('--optimizers_hpo', type=str, nargs='+', help='Optimizers to sample from in HPO')
+    parser.add_argument('--resume_hpo', action='store_true', help='Whether or not to resume an HPO trial')
+    parser.add_argument('--jobs', type=int, help='Number of cores to use for parsl')
+    parser.add_argument('--data_workers', type=int, help='Number of processes to run for dataloading')
+    parser.add_argument('--name', type=str, help='Name of the model')
+    parser.add_argument('--graph', action='store_true', help='Whether or not to save a plot of the results')
+    parser.add_argument('--run_dir', type=str, help='Directory to use for parsl')
+    parser.add_argument('--loss', type=str, help='Loss function to use.')
+    parser.add_argument('--version', type=str, help='Version of the network (for MatCNN compatibility)')
+    parser.add_argument('--patience', type=int, help='Use patience after how many epochs')
+    parser.add_argument('--image_size', type=int, nargs=2, help='Size of images processed as x y (necessary for evaluate re-stitching)')
+    parser.add_argument('--num_patches', type=int, nargs='+', help='Number of patches each slice is split into. Set to 0 for automatic choice based on patch and image size.')
+    parser.add_argument('--overlap_size', type=int, help='Width/height of the overlap between each patch')
+    parser.add_argument('--slices', type=int, help='Number of slices to take at a time')
+    parser.add_argument('--no-overlay', help='On evaluate, create full stitched images rather than overlays (if applicable)', dest='overlay', action='store_false')
+    parser.add_argument('--data-split', type=float, nargs=3, help="3 floats for test validation train split of data in image2npy (in order test, validation, train)", dest='split')
+    parser.add_argument('--collect', action='store_true', help='Whether or not to manually garbage collect each input')
+    parser.add_argument('--patching_mode', type=str, help="Mode with which to patch images. Either \"grid\" or \"random\"")
+    parser.add_argument('--clear_predictions', action='store_true', help='Clear prediction patches during evaluate')
+    parser.add_argument('--tar', action='store_true', help='Tar output when done')
+    parser.add_argument('--sampling_slices_hpo', type=int, nargs='+', help='Numbers of slices to use for models in HPO')
+    parser.add_argument('--sampling_patching_modes_hpo', type=str, nargs='+', help='Patching modes to sample with in HPO')
+    parser.add_argument('--min_overlap_size_hpo', type=int, help='Smallest size of overlap in HPO')
+    parser.add_argument('--max_overlap_size_hpo', type=int, help='Maximum size of overlap in HPO')
+    parser.add_argument('--mask', type=str, help='Mask to apply when getting metrics')
+    parser.add_argument('--losses', type=str, nargs='+', help='Losses to sample from in HPO')
+    parser.add_argument('--train_set', type=str, help='Dataset to use only as train')
+    parser.add_argument('--test_set', type=str, help='Dataset to use only as test')
+    parser.add_argument('--val_set', type=str, help='Dataset to use only as validation')
+    parser.add_argument('--blocks', type=int, help='Number of encoding blocks to use in each architecture')
+    parser.add_argument('--patch_size', type=int, help='Size of patches to use for target generation. Set to -1 to disable patching')
+    parser.add_argument('--data_split', type=float, nargs=3, help="3 floats for test validation train split of data in image2npy (in order test, validation, train)", dest='split')
+    parser.add_argument('--gamma', type=float, help='Amount to decrease LR by every 3 epochs')
+    parser.add_argument('--imagenet_stats', action='store_true', help='Use ImageNet stats instead of dataset stats for normalization')
+    parser.add_argument('--stats_file', type=str, help='JSON file to read stats from')
+    parser.add_argument('--video', type=str, help='Video file to perform inference on')
+    parser.add_argument('--video_dir', type=str, help='Directory containing videos. Recursively searches for videos')
+    parser.add_argument('--simclr_checkpoint', type=str, help='Weights for SimCLR pre-trained ResNet. Only needed if training a simclr_faster_rcnn.')
+    parser.add_argument('--augment_out', type=str, help='Output directory for augment mode')
+
+    args = parser.parse_args()
     if args.config:
         if os.path.isfile(args.config):
             configs = [args.config]
